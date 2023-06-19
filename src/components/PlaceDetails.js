95517546e441d40adcbd645f7dc9b39d9c91ce2d
IgorFurman <123864445+IgorFurman@users.noreply.github.com>
2023-06-19 15:38:53 +0200

    slider for opinions added

--- src/components/PlaceDetails.js
+++ src/components/PlaceDetails.js
@@ -1,6 +1,10 @@
 import React, { useEffect } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 import {
 	ButtonList,
 	PlaceDetailsStyled,
@@ -13,15 +17,17 @@ import {
 } from '../styles/styles';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { addToVisit, } from '../redux/placesToVisitSlice';
+import { addToVisit } from '../redux/placesToVisitSlice';
 
-import { FETCH_PLACE_DETAILS } from '../redux/sagas'; 
+import { FETCH_PLACE_DETAILS } from '../redux/sagas';
 
 const PlaceDetails = ({ style }) => {
 	const dispatch = useDispatch();
 	const placesToVisit = useSelector((state) => state.placesToVisit);
 
-  const selectedPlace = useSelector(state => state.placesDisplay.selectedPlace);
+	const selectedPlace = useSelector(
+		(state) => state.placesDisplay.selectedPlace
+	);
 
 	const handleAddToVisit = (place) => {
 		dispatch(addToVisit(place));
@@ -29,15 +35,74 @@ const PlaceDetails = ({ style }) => {
 
 	const isPlaceInVisitList = (placeId) => {
 		return placesToVisit.some((place) => place.place_id === placeId);
-	}
+	};
 
 	useEffect(() => {
 		if (selectedPlace && selectedPlace.place_id) {
 			dispatch({ type: FETCH_PLACE_DETAILS, payload: selectedPlace.place_id });
 		}
 	}, [selectedPlace?.place_id, dispatch]);
-	
 
+	const sliderOpinionsSettings = {
+		dots: false,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 8000,
+		arrows: true,
+		nextArrow: <NextArrow />,
+		prevArrow: <PrevArrow />,
+	};
+
+	function PrevArrow(props) {
+		const { className, style, onClick } = props;
+		return (
+			<div
+				className={className}
+				style={{
+					...style,
+					display: "block",
+					color: "gray",
+					fontSize: "40px",
+					lineHeight: "1",
+					position: "absolute",
+					top: "50%",
+					left: "-36px",
+					transform: "translateY(-50%)",
+					cursor: "pointer",
+				}}
+				onClick={onClick}
+			>
+				&#8249;
+			</div>
+		);
+	}
+	
+	function NextArrow(props) {
+		const { className, style, onClick } = props;
+		return (
+			<div
+				className={className}
+				style={{
+					...style,
+					display: "block",
+					color: "gray",
+					fontSize: "40px",
+					lineHeight: "1",
+					position: "absolute",
+					top: "50%",
+					right: "-2px",
+					transform: "translateY(-50%)",
+					cursor: "pointer",
+				}}
+				onClick={onClick}
+			>
+				&#8250;
+			</div>
+		);
+	}
 
 	if (!selectedPlace) {
 		return (
@@ -54,23 +119,28 @@ const PlaceDetails = ({ style }) => {
 			<h2>{selectedPlace.name}</h2>
 			<p>
 				<b>Adres: </b>
-				{selectedPlace.formatted_address}
+				{selectedPlace.formatted_address
+					? selectedPlace.formatted_address
+					: 'Brak adresu'}
 			</p>
 			<p>
 				<b>Numer: </b>
-				{selectedPlace.formatted_phone_number}
+				{selectedPlace.formatted_phone_number
+					? selectedPlace.formatted_phone_number
+					: 'Brak numeru'}
 			</p>
 
-			{selectedPlace.opening_hours && selectedPlace.opening_hours.weekday_text && (
-	<OpeningHours>
-		<p>Godziny otwarcia:</p>
-		<ul>
-			{selectedPlace.opening_hours.weekday_text.map((day, index) => (
-				<li key={index}>{day}</li>
-			))}
-		</ul>
-	</OpeningHours>
-)}
+			{selectedPlace.opening_hours &&
+				selectedPlace.opening_hours.weekday_text && (
+					<OpeningHours>
+						<p>Godziny otwarcia:</p>
+						<ul>
+							{selectedPlace.opening_hours.weekday_text.map((day, index) => (
+								<li key={index}>{day}</li>
+							))}
+						</ul>
+					</OpeningHours>
+				)}
 
 			{selectedPlace.website && (
 				<p>
@@ -79,61 +149,69 @@ const PlaceDetails = ({ style }) => {
 						href={selectedPlace.website}
 						rel='noopener noreferrer'
 					>
-						Dowiedz się więcej
+						Odwiedź stronę internetową
 					</WebsideLink>
 				</p>
 			)}
 
-{selectedPlace.photos && selectedPlace.photos.length > 0 ? (
-  <CarouselWrapper>
-    <Carousel
-      autoPlay
-      dynamicHeight
-      emulateTouch
-      infiniteLoop
-      interval={3000}
-      showArrows
-      showStatus={false}
-      showIndicators
-      stopOnHover
-      swipeable
-      transitionTime={350}
-      useKeyboardArrows
-      showThumbs={false}
-    >
-      {selectedPlace.photos.map((photo, index) => (
-        <CarouselItem key={index}>
-          <CarouselImage
-            src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=1000&photoreference=${photo.photo_reference}&key=${process.env.REACT_APP_GOOGLE_PLACES_API_KEY}`}
-            alt={selectedPlace.name}
-          />
-        </CarouselItem>
-      ))}
-    </Carousel>
-  </CarouselWrapper>
-) : (
-  <p>Brak zdjęcia</p>
-)}
+			{selectedPlace.photos && selectedPlace.photos.length > 0 ? (
+				<CarouselWrapper>
+					<Carousel
+						autoPlay
+						dynamicHeight
+						emulateTouch
+						infiniteLoop
+						interval={3000}
+						showArrows
+						showStatus={false}
+						showIndicators
+						stopOnHover
+						swipeable
+						transitionTime={350}
+						useKeyboardArrows
+						showThumbs={false}
+					>
+						{selectedPlace.photos.map((photo, index) => (
+							<CarouselItem key={index}>
+								<CarouselImage
+									src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=1000&photoreference=${photo.photo_reference}&key=${process.env.REACT_APP_GOOGLE_PLACES_API_KEY}`}
+									alt={selectedPlace.name}
+								/>
+							</CarouselItem>
+						))}
+					</Carousel>
+				</CarouselWrapper>
+			) : (
+				<p>Brak zdjęcia</p>
+			)}
 
-			{selectedPlace.rating && <p>Ocena ogólna: {selectedPlace.rating}⭐</p>}
+			{selectedPlace.rating && (
+				<p>
+					<b>Ocena ogólna: </b>
+					{selectedPlace.rating}⭐
+				</p>
+			)}
 
 			{selectedPlace.reviews && selectedPlace.reviews.length > 0 ? (
-  <OpinionsWrapper>
-    <h3>Opinie:</h3>
-    {selectedPlace.reviews.map((review, index) => (
-      <div key={index}>
-        <p>
-          {review.author_name} ({review.rating}⭐):
-        </p>
-        <p>{review.text}</p>
-      </div>
-    ))}
-  </OpinionsWrapper>
-) : (
-  <p>Brak opinii</p>
-)}
+				<OpinionsWrapper>
+					<h3>Opinie:</h3>
+					<Slider {...sliderOpinionsSettings} >
+						{selectedPlace.reviews.map((review, index) => (
+							<div key={index}>
+								<p>
+									{review.author_name} ({review.rating}⭐):
+								</p>
+								<p>{review.text}</p>
+							</div>
+						))}
+					</Slider>
+				</OpinionsWrapper>
+			) : (
+				<p>Brak opinii</p>
+			)}
 
 			<ButtonList
+			style={{ margin: "50px 0 0 0"}}
 				onClick={() => handleAddToVisit(selectedPlace)}
 				disabled={isPlaceInVisitList(selectedPlace.place_id)}
 			>
